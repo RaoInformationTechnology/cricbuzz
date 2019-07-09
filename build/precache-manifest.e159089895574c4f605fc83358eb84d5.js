17a820379c70c07c5728edac4aea731eed6c44d5
VM8198 <malvivivek8198@gmail.com>
2019-07-09 18:39:49 +0530

    production build

---
+++ build/precache-manifest.e159089895574c4f605fc83358eb84d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c823b45662597379f3d8ea6257e93cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21d7035c83e84181f141",
+    "url": "/static/css/main.12388eb4.chunk.css"
+  },
+  {
+    "revision": "030025fed448b4ef3319",
+    "url": "/static/js/2.e0ea9faa.chunk.js"
+  },
+  {
+    "revision": "21d7035c83e84181f141",
+    "url": "/static/js/main.e37adc14.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9c6da1ba5cb56116baf6bb6f256ead5f",
+    "url": "/static/media/bg.9c6da1ba.jpg"
+  },
+  {
+    "revision": "00b90c50daefb9238625ba25374fa48b",
+    "url": "/static/media/loader.00b90c50.gif"
+  },
+  {
+    "revision": "cac29473595e25d9a5e35f14bc9e0cec",
+    "url": "/static/media/player_bg1.cac29473.jpg"
+  }
+]);
